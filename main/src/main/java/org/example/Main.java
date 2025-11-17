fb68b8209e0409d299255253b39b4f00705bb284
Diego Jimenez <diego.jimenez@noseryoung.ch>
2025-11-17 11:45:30 +0100

    add question for portfolio

---
+++ main/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+  static void main() {
+
+    IO.println(String.format("Hello and welcome!"));
+    IO.println("This is a simple Java program.");
+    system.out.println("Goodbye!");
+  }
+}
